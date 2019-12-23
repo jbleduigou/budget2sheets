a7296f98185ffca06fa9cc08ceed765a61ed79cb
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2019-12-23 18:23:23 +0100

    first version working

---
+++ authentication/token.go
@@ -0,0 +1,26 @@
+package authentication
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+func GetToken() *oauth2.Token {
+	tok := &oauth2.Token{}
+	accessToken64, ok := os.LookupEnv("GOOGLE_ACCESS_TOKEN")
+	if ok {
+		decoded, err := base64.StdEncoding.DecodeString(accessToken64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tok.AccessToken = string(decoded)
+	}
+	tok.TokenType = "Bearer"
+	tok.RefreshToken = os.Getenv("GOOGLE_REFRESH_TOKEN")
+	tok.Expiry = time.Now()
+	return tok
+}
--- cmd/budget2sheets/main.go
+++ cmd/budget2sheets/main.go
@@ -1,22 +1,95 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/jbleduigou/budget2sheets/authentication"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
+func getClient() (*http.Client, error) {
+	config, err := google.ConfigFromJSON([]byte(authentication.GetCredentials()), "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		return nil, err
+	}
+	return config.Client(context.Background(), authentication.GetToken()), nil
+}
+
 func handler(ctx context.Context, s3Event events.S3Event) {
-	// Create all collaborators for command
+	client, err := getClient()
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	spreadsheetID := os.Getenv("GOOGLE_SPREADSHEET_ID")
+	writeRange := "Suivi Dépenses Janvier!A2"
+
+	for _, record := range s3Event.Records {
+		vr, _ := readData(record.S3.Bucket.Name, record.S3.Object.Key)
+
+		_, err = srv.Spreadsheets.Values.Append(spreadsheetID, writeRange, &vr).ValueInputOption("USER_ENTERED").Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve data from sheet. %v", err)
+		}
+	}
+}
+
+func readData(bucketName string, objectKey string) (sheets.ValueRange, error) {
+	var vr sheets.ValueRange
+
+	content, _ := downloadFile(bucketName, objectKey)
+	reader := csv.NewReader(bytes.NewReader(content))
+
+	rawCSVdata, err := reader.ReadAll()
+
+	if err != nil {
+		return vr, err
+	}
+
+	for _, each := range rawCSVdata {
+		if len(each) == 5 {
+			euro, _ := strconv.ParseFloat(string(each[4]), 64)
+			myval := []interface{}{each[0], each[1], each[2], each[3], euro}
+			vr.Values = append(vr.Values, myval)
+		}
+	}
+	return vr, nil
+}
+
+func downloadFile(bucketName string, objectKey string) ([]byte, error) {
 	sess := session.Must(session.NewSession())
-	fmt.Println(sess)
+	downloader := s3manager.NewDownloader(sess)
+	fmt.Printf("Downloading file '%v' from bucket '%v' \n", objectKey, bucketName)
+	buff := &aws.WriteAtBuffer{}
+	n, err := downloader.Download(buff, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		fmt.Printf("Failed to download file %v\n, %v", objectKey, err)
+		return nil, err
+	}
+	fmt.Printf("File %v downloaded, read %d bytes\n", objectKey, n)
+	return buff.Bytes(), nil
 }
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
-	// lambda.Start(handler)
-
-	fmt.Println("Hello gophers!")
+	lambda.Start(handler)
 }
