889ff43e86d1a606ca4a79d4156b5dfcfb49103c
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2020-01-30 21:49:24 +0100

    Extracting config logic to a dedicated folder
    Signed-off-by: Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>

--- cmd/budget2sheets/main.go
+++ cmd/budget2sheets/main.go
@@ -4,26 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jbleduigou/budget2sheets/authentication"
+	"github.com/jbleduigou/budget2sheets/config"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
 func getClient() (*http.Client, error) {
-	config, err := google.ConfigFromJSON([]byte(authentication.GetCredentials()), "https://www.googleapis.com/auth/spreadsheets")
+	oauth2, err := google.ConfigFromJSON([]byte(authentication.GetCredentials()), "https://www.googleapis.com/auth/spreadsheets")
 	if err != nil {
 		return nil, err
 	}
-	return config.Client(context.Background(), authentication.GetToken()), nil
+	return oauth2.Client(context.Background(), authentication.GetToken()), nil
 }
 
 func handler(ctx context.Context, event events.SQSEvent) {
+	config := config.NewConfiguration()
 	client, err := getClient()
 
 	srv, err := sheets.New(client)
@@ -31,12 +32,9 @@ func handler(ctx context.Context, event events.SQSEvent) {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
-	spreadsheetID := os.Getenv("GOOGLE_SPREADSHEET_ID")
-	writeRange := os.Getenv("GOOGLE_SPREADSHEET_RANGE") // "Suivi Dépenses Janvier!A2"
-
 	for _, record := range event.Records {
 		vr, _ := extractData(record)
-		_, err = srv.Spreadsheets.Values.Append(spreadsheetID, writeRange, &vr).ValueInputOption("USER_ENTERED").InsertDataOption("INSERT_ROWS").Do()
+		_, err = srv.Spreadsheets.Values.Append(config.GetSpreadSheetID(), config.GetWriteRange(), &vr).ValueInputOption("USER_ENTERED").InsertDataOption("INSERT_ROWS").Do()
 		if err != nil {
 			log.Fatalf("Unable to retrieve data from sheet. %v", err)
 		}
@@ -47,8 +45,10 @@ func extractData(m events.SQSMessage) (sheets.ValueRange, error) {
 	fmt.Printf("Processing SQS message with id '%v'\n", m.MessageId)
 	var vr sheets.ValueRange
 	euro, _ := strconv.ParseFloat(*m.MessageAttributes["Value"].StringValue, 64)
-	myval := []interface{}{*m.MessageAttributes["Date"].StringValue, *m.MessageAttributes["Description"].StringValue, "", *m.MessageAttributes["Category"].StringValue, euro}
+	description := *m.MessageAttributes["Description"].StringValue
+	myval := []interface{}{*m.MessageAttributes["Date"].StringValue, description, "", *m.MessageAttributes["Category"].StringValue, euro}
 	vr.Values = append(vr.Values, myval)
+	fmt.Printf("Description of transaction is '%v'\n", description)
 	return vr, nil
 }
 
