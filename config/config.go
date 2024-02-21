7c37d6645d523c82eabef0406dbee618ddc44114
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2024-02-21 20:29:21 +0000

    feat: 💥 use Google service account

--- config/config.go
+++ config/config.go
@@ -2,34 +2,70 @@ package config
 
 import (
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"go.uber.org/zap"
+	"golang.org/x/net/context"
 )
 
 // Configuration provides and interface for the configuration of the software
 type Configuration interface {
 	GetSpreadSheetID() string
 	GetWriteRange() string
-	GetGoogleClientID() string
-	GetGoogleProjectID() string
-	GetGoogleClientSecret() string
+	GetGoogleJsonCredentials() []byte
+}
+
+type SecretsManager interface {
+	GetSecretValue(ctx context.Context, params *secretsmanager.GetSecretValueInput, optFns ...func(*secretsmanager.Options)) (*secretsmanager.GetSecretValueOutput, error)
 }
 
 // NewConfiguration will provide an instance of a Configuration, implementation is not exposed
-func NewConfiguration() Configuration {
-	return &configuration{
-		spreadSheetID:      os.Getenv("GOOGLE_SPREADSHEET_ID"),
-		writeRange:         os.Getenv("GOOGLE_SPREADSHEET_RANGE"),
-		googleClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		googleProjectID:    os.Getenv("GOOGLE_PROJECT_ID"),
-		googleClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+func NewConfiguration(ctx context.Context) (Configuration, error) {
+	svc, err := initSecretsManagerClient(ctx)
+	if err != nil {
+		return nil, err
 	}
+
+	c, err := retrieveCredentialsFromSecret(ctx, svc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &configuration{
+		spreadSheetID:         os.Getenv("GOOGLE_SPREADSHEET_ID"),
+		writeRange:            os.Getenv("GOOGLE_SPREADSHEET_RANGE"),
+		googleJsonCredentials: c,
+	}, nil
+}
+
+func initSecretsManagerClient(ctx context.Context) (SecretsManager, error) {
+	zap.L().Info("Creating Secrets Manager client")
+	awsCfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+	svc := secretsmanager.NewFromConfig(awsCfg)
+	return svc, nil
+}
+
+func retrieveCredentialsFromSecret(ctx context.Context, svc SecretsManager) ([]byte, error) {
+	arn := os.Getenv("GOOGLE_CREDENTIALS_SECRET_ARN")
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(arn),
+	}
+	zap.L().Info("Retrieving secret",
+		zap.String("secret-arn", arn))
+	result, err := svc.GetSecretValue(ctx, input)
+
+	return []byte(*result.SecretString), err
 }
 
 type configuration struct {
-	spreadSheetID      string
-	writeRange         string
-	googleClientID     string
-	googleProjectID    string
-	googleClientSecret string
+	spreadSheetID         string
+	writeRange            string
+	googleJsonCredentials []byte
 }
 
 func (c *configuration) GetSpreadSheetID() string {
@@ -40,14 +76,6 @@ func (c *configuration) GetWriteRange() string {
 	return c.writeRange
 }
 
-func (c *configuration) GetGoogleClientID() string {
-	return c.GetGoogleClientID()
-}
-
-func (c *configuration) GetGoogleProjectID() string {
-	return c.GetGoogleProjectID()
-}
-
-func (c *configuration) GetGoogleClientSecret() string {
-	return c.GetGoogleClientSecret()
+func (c *configuration) GetGoogleJsonCredentials() []byte {
+	return c.googleJsonCredentials 
 }
